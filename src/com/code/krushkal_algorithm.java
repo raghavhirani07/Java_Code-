1f5f8dc793089fec91eebd0c0ae6be6be5b2f2e2
RAGHAV HIRANI <raghavhirani07@gmail.com>
2022-10-04 12:31:34 +0530

    commit krushkal

--- src/com/code/krushkal_algorithm.java
+++ src/com/code/krushkal_algorithm.java
@@ -1,2 +1,11 @@
-package com.code;public class krushkal_algorithm {
+package com.code;
+
+import java.util.Set;
+
+public class krushkal_algorithm {
+    public static void main(String[] args) {
+        int[][] graph_weight = {{1,1,1,0},{0,0,0,1},{0,0,0,1},{0,0,0,0}};
+        char[] vertex = {'A','B','C','D','E'};
+
+    }
 }
