89641eb214795b80c59a001be74282eef18fba9d
RAGHAV HIRANI <raghavhirani07@gmail.com>
2022-10-05 22:19:28 +0530

    Add sorting in edge kruskal

--- src/com/code/krushkal_algorithm.java
+++ src/com/code/krushkal_algorithm.java
@@ -1,17 +1,17 @@
 package com.code;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.Comparator;
 
 public class krushkal_algorithm {
     public static void main(String[] args) {
-        int[][] graph_weight = {{1,1,1,0},{0,0,0,1},{0,0,0,1},{0,0,0,0}};
+        int[][] graph_weight = {{0,7,8,5,0},{7,0,0,10,6},{8,0,0,9,0},{5,10,9,3,15},{0,6,0,15,0}};
         char[] vertex = {'A','B','C','D','E'};
-        int[][] arr = new  int[10][3];
-        int count = 0;
+        int l = 20 ;
+        int[][] arr = new  int[l][3];
+         int count = 0;
         for (int i = 0; i < graph_weight.length ; i++) {
-            for (int j = 0; j < graph_weight.length; j++) {
+            for (int j = i + 1; j < graph_weight.length; j++) {
                 if (graph_weight[i][j] != 0) {
                     arr[count][0] = (int) vertex[i];
                     arr[count][1] = (int) vertex[j];
@@ -20,6 +20,11 @@ public class krushkal_algorithm {
                 }
             }
         }
-
+        Arrays.sort (arr, (int[] o1 , int[] o2) -> o2[2] - o1[2]);
+        for (int i = 0; i < count ; i++) {
+            System.out.print ("Edge {"+ (char)arr[i][0]+","+(char)arr[i][1]+"} = w { "+ arr[i][2]+" }");
+            System.out.println ();
+        }
+        // Sorting array to it's weight
     }
 }
