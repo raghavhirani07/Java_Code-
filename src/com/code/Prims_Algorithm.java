940d3df97b121a5e7a4066f2bf55bbc4c6d37acd
RAGHAV HIRANI <raghavhirani07@gmail.com>
2022-10-06 13:54:44 +0530

    First commit of prims

---
+++ src/com/code/Prims_Algorithm.java
@@ -0,0 +1,14 @@
+package com.code;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
+
+public class Prims_Algorithm {
+
+    public static void main(String[] args) {
+        int[][] graph = {{0, 7, 8, 5, 0}, {7, 0, 0, 10, 6}, {8, 0, 0, 9, 0}, {5, 10, 9, 3, 15}, {0, 6, 0, 15, 0}};
+        char[] vertex = {'A', 'B', 'C', 'D', 'E'};
+
+    }
+}
