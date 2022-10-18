b17d874ad9b04a25df04b498f8813bdca611c1e2
RAGHAV HIRANI <raghavhirani07@gmail.com>
2022-10-18 14:13:06 +0530

    "create patten_3 "

---
+++ src/com/code/pattern_3.java
@@ -0,0 +1,16 @@
+package com.code;
+
+public class pattern_3 {
+    //? Inverted half pyramid With Numbers
+    public static void pyramid_with_numbers(int col,int row){
+        for (int i = 0; i < row ; i++) {
+            for (int j = 0; j <col-i; j++) {
+                System.out.print (j+1);
+            }
+            System.out.println ();
+        }
+    }
+    public static void main(String[] args) {
+        pyramid_with_numbers (5,5);
+    }
+}
