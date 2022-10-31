a7f744add83a3baa727d6cb36ae32c8f19c0f517
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2022-10-31 14:42:38 +0530

    Update
    "Add a Optimum way to do Example "

--- src/com/code/Arry_list_ex3.java
+++ src/com/code/Arry_list_ex3.java
@@ -3,11 +3,31 @@ package com.code;
 import java.util.ArrayList;
 
 public class Arry_list_ex3 {
+    //* Bruth Force Apoorach
     public static  int storewater (ArrayList<Integer> arr ){
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.size () ; i++) {
             for (int j = i+1; j < arr.size () ; j++) {
                 max = Math.max (Math.min(arr.get (i), arr.get (j))*(j-i),max);
+            }
+        }
+        return max;
+    }
+    //* optimum approach o(n)
+    public  static  int storewater1(ArrayList<Integer> arr ){
+        int max = 0;
+        int left = 0;
+        int right = arr.size ()-1;
+        while (left < right){
+            int hight = Math.min(arr.get (left),arr.get (right));
+            int width = right - left;
+            max = Math.max (max,hight*width);
+            if(arr.get (left)  < arr.get (right)){
+                left++;
+            }
+            else
+            {
+                right--;
             }
         }
         return max;
@@ -25,5 +45,6 @@ public class Arry_list_ex3 {
         arr.add (3);
         arr.add (7);
         System.out.println (storewater (arr));
+        System.out.println (storewater1 (arr));
     }
 }
