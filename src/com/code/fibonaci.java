c9bb98ce534507ac87e8448096914c1e3a394641
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2022-11-03 15:41:50 +0530

    Create
    "Create a problem and done with fibonaci"

---
+++ src/com/code/fibonaci.java
@@ -0,0 +1,18 @@
+package com.code;
+
+public class fibonaci{
+    public  static  int fibo(int n ){
+        if( n == 0  || n == 1){
+            return n;
+        }
+        return  fibo (n-1)+fibo (n-2);
+
+
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println (fibo (i));
+        }
+
+    }
+}
