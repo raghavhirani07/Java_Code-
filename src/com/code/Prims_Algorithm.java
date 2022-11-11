aa2e385bdaae028476d4da81f401c878aa21cd40
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2022-11-11 18:53:30 +0530

    Complete prims
    add more function and complete the solution with  new graph class

--- src/com/code/Prims_Algorithm.java
+++ src/com/code/Prims_Algorithm.java
@@ -1,41 +1,50 @@
 package com.code;
-
 import java.util.*;
 
 public class Prims_Algorithm {
 
-    public static class graph{
-        char e1,e2;
-        int wight;
-        graph(char e1,char e2,int wight){
-           this.e1=e1;
-           this.e2=e2;
-           this.wight=wight;
-        }
-    }
     public static void connect_edge(int n ,ArrayList<graph> arr,int[][] g,char[] v){
         for (int i = n+1; i < 5 ; i++) {
             if(g[n][i] > 0){
                 arr.add (new graph (v[n],v[i],g[n][i]));
             }
         }
-        Collections.sort (arr, Comparator.comparingInt (o -> o.wight));
+        arr.sort (Comparator.comparingInt (o -> o.wight));
     }
-    public static boolean  iscreatecycle(Set<Character> s , char a){
+    public static boolean  iscreatecycle( Set<Character> s , char a){
         int len  = s.size ();
         s.add (a);
-        return len == s.size ();
+        return (len == s.size ());
+    }
+    public static char selectEdge(ArrayList<graph> arr , Set<Character> s , char a ) {
+        char n =a;
+        for (int j = 0; j < arr.size (); j++) {
+            if (!iscreatecycle (s, arr.get (j).e2)) {
+                System.out.println (arr.get (j).e1 + "--> " + arr.get (j).e2);
+                n = arr.get (j).e2;
+                s.add (n);
+                arr.remove (j);
+            }
+        }
+        return n;
+    }
+    public static int indexofarray(char[] arr , char c){
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i] == c){
+                return i;
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
         int[][] g = {{0, 7, 8, 5, 0}, {7, 0, 0, 10, 6}, {8, 0, 0, 9, 0}, {5, 10, 9, 3, 15}, {0, 6, 0, 15, 0}};
         char[] vertex = {'A', 'B', 'C', 'D', 'E'};
         ArrayList<graph> arr = new ArrayList<> ();
-        connect_edge (0,arr,g,vertex);
         Set<Character> select = new HashSet<> ();
-        Iterator<graph> i = arr.iterator();
-        while(i.hasNext ()){
-            System.out.println (i.next ().wight);
+        char temp ='A';
+        while(select.size () != vertex.length){
+             temp =selectEdge (arr,select,temp);
+             connect_edge (indexofarray (vertex,temp), arr,g,vertex);
         }
-
     }
 }
--- src/com/code/graph.java
+++ src/com/code/graph.java
@@ -1,5 +1,11 @@
 package com.code;
 
 public class graph {
-
+    char e1,e2;
+    int wight;
+    graph(char e1,char e2,int wight){
+        this.e1=e1;
+        this.e2=e2;
+        this.wight=wight;
+    }
 }
